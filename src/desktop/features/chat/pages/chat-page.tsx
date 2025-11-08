b7ff0a2b470ac2b3a267e304a02291ff0c60b78a
Peiiii <wangpeiiii@qq.com>
2025-11-08 15:31:29 +0800

    feat(chat): disable settings panel in chat page

--- src/desktop/features/chat/pages/chat-page.tsx
+++ src/desktop/features/chat/pages/chat-page.tsx
@@ -60,7 +60,7 @@ export function ChatPage() {
                                     <DiscussionController
                                         status={status}
                                         onToggleMembers={handleToggleMembers}
-                                        // enableSettings={false}
+                                        enableSettings={false}
                                     />
                                 )}
                                 <div className="flex-1 min-h-0">
